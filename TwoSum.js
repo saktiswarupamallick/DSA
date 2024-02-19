0c9b1738d6bc0d90bfd66552266b9a2a4a604e30
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-19 17:54:40 +0000

    #

---
+++ TwoSum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} num
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(num, target) {
+    let n = num.length;
+    for(let i=0 ;i<= n-2; i++){
+        for(let j =i+1;j <n; j++){
+            if(num[i] +num[j] == target){
+              return [i,j]
+            }
+           
+        }
+    }
+};
