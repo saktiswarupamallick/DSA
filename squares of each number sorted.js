65eea2009369e3a292d0da17d285ee6e0581752f
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-23 06:17:31 +0000

    #

---
+++ squares of each number sorted.js
@@ -0,0 +1,4 @@
+//sort function sort the array in dictionary order ..not ascending   so we have give parameters to that funct
+//arr.sort((x,y)=>x-y);//this sort correctly ascend
+//-ve x<y //+ve x>y
+//arr.sort((x,y)=>y-x); //descend
