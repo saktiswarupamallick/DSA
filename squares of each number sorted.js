5ff9c0573403d9750eb5f388dbd5a2ad88bccddb
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-23 06:43:55 +0000

    #

--- squares of each number sorted.js
+++ squares of each number sorted.js
@@ -4,3 +4,9 @@
 //-ve x<y(1st arg puts ffirst then 2nd) //+ve x>y (vice versa)
 //arr.sort((x,y)=>y-x); //descend
 //sort takes lot of instructions
+
+
+
+//one of the extreme ends is going to give largest element
+//the greater one will fill the sits  from back 
+// 2 pointers i and j will come inside accod to which value went to fulfil the space
