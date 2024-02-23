0ee511003f893c323820254e355d874376bd5c59
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-23 17:18:31 +0000

    #

--- squares of each number sorted.js
+++ squares of each number sorted.js
@@ -10,3 +10,26 @@
 //one of the extreme ends is going to give largest element
 //the greater one will fill the sits  from back 
 // 2 pointers i and j will come inside accod to which value went to fulfil the space
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+    let n = nums.length; 
+     let result = Array(n);
+     let left = 0;
+     let right = n-1;
+  
+     for( let i=n-1; i>=0;i--){
+         if(nums[left]**2 > nums[right]**2){
+             result[i] = nums[left]**2;
+             left ++; 
+         }else{
+             result[i] = nums[right]**2;
+             right --;
+         }
+     }
+     return result
+     
+  };
