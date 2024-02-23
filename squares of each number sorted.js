d2fafbb8b0959c41d26eb6e4b902b0d4504f8354
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-02-23 06:21:15 +0000

    #

--- squares of each number sorted.js
+++ squares of each number sorted.js
@@ -3,3 +3,4 @@
 //arr.sort((x,y)=>x-y);//this sort correctly ascend
 //-ve x<y(1st arg puts ffirst then 2nd) //+ve x>y (vice versa)
 //arr.sort((x,y)=>y-x); //descend
+//sort takes lot of instructions
