a0daaee3ab4bfe26737b1cad2d4b7b2209227fe9
Sakti Swarupa Mallick <152704778+saktiswarupamallick@users.noreply.github.com>
2024-03-04 18:20:03 +0000

    #

--- selectionsort.js
+++ selectionsort.js
@@ -5,6 +5,8 @@ function selectionsort(){
 function min_element(arr,i){
     let min_element = i;
     for(j=i+1; j <arr.length; j++ ) {
-        
+        if(arr[j] < arr[min_element]){
+            min_element =j
+        }
     }
 }
